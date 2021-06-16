f5abd9e87bfeb7415486ed9143347feb0375572c
Thiago Felipe <thiago.felipe@tutanota.com>
2021-06-16 11:23:16 -0400

    Refatorando a página home

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,7 @@ const URL = " https://pokeapi.co/api/v2";
 export function getPokemon(pokemonID) {
   return axios.get(`${URL}/pokemon/${pokemonID}`);
 }
+
+export function getPokemonList() {
+  return axios.get(`${URL}/pokemon?limit=20`);
+}
