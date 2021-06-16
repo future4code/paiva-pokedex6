39f92f89710b1af9cc783d007f393d9246d1d401
Thiago Felipe <thiago.felipe@tutanota.com>
2021-06-16 11:33:27 -0400

    Refatorando a página PokemonDetails

--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -1,19 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import PokemonDetails from "../components/PokemonDetail";
-import useGetPokemon from "../hooks/useGetPokemon";
 
 export default function Details() {
   const { id } = useParams();
-  const { pokemon, loading, error } = useGetPokemon(id);
-  let show;
-
-  if (loading)
-    show = <p>Carregando o Pokémon</p>;
-  else if (error)
-    show = <p>Ocorreu um erro ao mostrar o Pokémon</p>;
-  else
-    show = <PokemonDetails pokemon={pokemon}/>;
+  const [ name, setName ] = useState("");
 
   return (
     <>
@@ -21,13 +12,13 @@ export default function Details() {
         <button>
           voltar
         </button>
-        <h1>{pokemon.name || "Detalhes Do Pokémon"}</h1>
+        <h1>{name || "Detalhes Do Pokémon"}</h1>
         <button>
           Adicionar/Remover
         </button>
       </header>
       <main>
-        {show}
+        <PokemonDetails id={id} setName={setName}/>
       </main>
     </>
   );
