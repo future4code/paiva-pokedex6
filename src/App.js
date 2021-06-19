b0861ab946b60411707b3e0ee582fdfb53142913
hgalvao98 <81254055+hgalvao98@users.noreply.github.com>
2021-06-19 13:14:23 -0300

    design (#15)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Router from "./routes/Router";
 import { GlobalState } from "./global/GlobalState";
+import GlobalStyle from './globalStyles';
+
 
 function App() {
   return (
     <GlobalState>
+      <GlobalStyle />
       <Router/>
     </GlobalState>
   );
--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router";
 import PokeCard from "../components/PokeCard";
 import { useGlobalStates } from "../global/GlobalState";
 import { goToHome } from "../routes/coodinator";
+import {Header, Main} from '../pages/Home/Styled'
 
 export default function Pokedex() {
   const history = useHistory();
@@ -10,13 +11,13 @@ export default function Pokedex() {
 
   return (
     <>
-      <header>
+      <Header>
         <button onClick={() => goToHome(history)}>Lista De Pokemons</button>
         <p>Pokedex</p>
-      </header>
-      <main>
+      </Header>
+      <Main>
         {pokedex.map((pokemon) => <PokeCard key={pokemon.name} pokemon={pokemon}/>)}
-      </main>
+      </Main>
     </>
   );
 }
